7b3032db93a45f2d1fb7639d243db2d470a37adb
maiosprog <mainor.ortiz.salguera@est.una.ac.cr>
2020-03-04 19:14:17 -0600

    Almost Final

--- app/src/main/java/com/minor/laboratorio2/MainActivity.java
+++ app/src/main/java/com/minor/laboratorio2/MainActivity.java
@@ -2,7 +2,10 @@ package com.minor.laboratorio2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //este cambio va para git
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, MessageActivity.class);
+        EditText editText=findViewById(R.id.text_message);
+
+        intent.putExtra("MESSAGE",editText.getText().toString());
+
+        startActivity(intent);
     }
 }
